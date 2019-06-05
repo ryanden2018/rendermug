d53faf5073f5655a2daa8645680cf35612c6ae01
ryanden2018 <denlinger.ryan@gmail.com>
2019-06-05 09:08:14 -0500

    gradual render

--- app/assets/javascripts/MugRenderer.js
+++ app/assets/javascripts/MugRenderer.js
@@ -5,6 +5,8 @@ function MugRenderer(width,numPhotons) {
   this.image = []
   this.width = width;
   this.numPhotons = numPhotons;
+  this.i = 0;
+  this.j = 0;
 
   for(var i=0; i < width*width; i++) {
     this.image.push(0.0);
@@ -29,58 +31,61 @@ MugRenderer.prototype.inMug = function(x,y,z) {
   return false;
 };
 
-MugRenderer.prototype.render = function() {
-  for(var i = 0; i < this.image.length; i++) {
-    this.image[i] = 0.0;
-  }
+MugRenderer.prototype.renderNextPixel = function() {
+  //console.log(`${this.i} ${this.j}`);
+  this.image[this.idx(this.i,this.j)] = 0.0;
 
-  for(var i=0; i<this.width; i++) {
-    for(var j = 0; j < this.width; j++) {
-      for(var k = 0; k < this.numPhotons; k++) {
-        var x = 12.0;
-        var y = 0.0;
-        var z = 6.0;
-        var vx = -1.0;
-        var vy = -0.5+(1.0*j)/this.width;
-        var vz = -(1.0*i)/this.width;
-        var numBounces = 0;
-        var dt = 0.1;
+  for(var k = 0; k < this.numPhotons; k++) {
+    var x = 8.0;
+    var y = 0.0;
+    var z = 12.0;
+    var vx = -1.0;
+    var vy = -0.5+(1.0*this.j)/this.width;
+    var vz = -1.0-(2.0*this.i)/this.width;
+    var numBounces = 0;
+    var dt = 0.05;
 
-        while((numBounces < this.maxBounces) &&
-            (Math.sqrt(Math.pow(x,2)+Math.pow(y,2)+Math.pow(z,2)) < 20))  {
+    while((numBounces < this.maxBounces) &&
+        (Math.sqrt(Math.pow(x,2)+Math.pow(y,2)+Math.pow(z,2)) < 20))  {
 
-          x += vx*dt;
-          y += vy*dt;
-          z += vz*dt;
+      x += vx*dt;
+      y += vy*dt;
+      z += vz*dt;
 
-          if( (Math.sqrt(Math.pow(x,2)+Math.pow(y,2)+Math.pow(z,2))>19.0) &&
-               ( vz > 0 ) &&
-               ( Math.sqrt(Math.pow(vx,2)+Math.pow(vy,2)) < 0.3 ) ) {
-            this.image[this.idx(i,j)] += Math.pow(this.decayFactor,numBounces);
+      if( (Math.sqrt(Math.pow(x,2)+Math.pow(y,2)+Math.pow(z,2))>19.0) &&
+            ( vz > 0 ) &&
+            ( Math.sqrt(Math.pow(vx,2)+Math.pow(vy,2)) < 0.3 ) ) {
+        this.image[this.idx(this.i,this.j)] += Math.pow(this.decayFactor,numBounces);
+        break;
+      
+      }
+
+      if(this.inMug(x,y,z)) {
+        numBounces += 1;
+        var m = 0;
+        while(true) {
+          m++;
+          vx = Math.random();
+          vy = Math.random();
+          vz = Math.random();
+          if( !this.inMug(x+3*vx*dt,y+3*vy*dt,z+3*vz*dt) ) {
             break;
-          
           }
-
-          if(this.inMug(x,y,z)) {
-            numBounces += 1;
-            var m = 0;
-            while(true) {
-              m++;
-              vx = Math.random();
-              vy = Math.random();
-              vz = Math.random();
-              if( !this.inMug(x+3*vx*dt,y+3*vy*dt,z+3*vz*dt) ) {
-                break;
-              }
-              if(m === 20) {
-                numBounces = this.maxBounces;
-                break;
-              }
-            }
+          if(m === 20) {
+            numBounces = this.maxBounces;
+            break;
           }
-
         }
       }
     }
+  }
+
+  this.j++;
+  if(this.j === this.width) {
+    this.i++;
+    this.j = 0;
+  }
+  if(this.i === this.width) {
+    this.i = 0;
   }
 };
--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -7,8 +7,7 @@ window.onload = function() {
   var context = canvas.getContext("2d");
   var width = canvas.width;
   var height = canvas.height;
-  var rm = new MugRenderer(width,5);
-  rm.render();
+  var rm = new MugRenderer(width/3,50);
 
 
   var imgdata = context.createImageData(width,height);
@@ -18,7 +17,7 @@ window.onload = function() {
     for(var i=0; i<width; i++) {
       for(var j=0; j<width; j++) {
         var idx0 = (i*width+j)*4;
-        var val = Math.min(Math.floor(rm.image[rm.idx(i,j)]*255),255);
+        var val = Math.min(Math.floor(rm.image[rm.idx(Math.floor(i/3),Math.floor(j/3))]*255),255);
         imgdata.data[idx0] = Math.floor(val);
         imgdata.data[idx0+1] = Math.floor(val);
         imgdata.data[idx0+2] = Math.floor(val);
@@ -29,6 +28,9 @@ window.onload = function() {
 
   function main(tf) {
     window.requestAnimationFrame(main);
+    for(var l = 0; l < 100; l++) {
+      rm.renderNextPixel();
+    }
     buildImg();
     context.putImageData(imgdata,0,0);
   }
