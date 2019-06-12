23ba2974cfbfca22b97d5b51b397be601e36dd0b
ryanden2018 <denlinger.ryan@gmail.com>
2019-06-11 21:04:05 -0500

    switch to high res after five second delay

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -6,7 +6,7 @@ window.onload = function() {
   var context = canvas.getContext("2d");
   var width = canvas.width;
   var height = canvas.height;
-  var rmHR = new MugRenderer(width,1);
+  var rmHR = new MugRenderer(width/2,1);
   rmHR.rotateX(-5*Math.PI/32);
   rmHR.rotateZ(-5*Math.PI/32);
   rmHR.reset();
@@ -21,7 +21,12 @@ window.onload = function() {
     for(var i=0; i<width; i++) {
       for(var j=0; j<width; j++) {
         var idx0 = (i*width+j)*4;
-        var val = Math.min((rmHR.image[width*i+j]/rmHR.maxVal)*255,255);
+        var val;
+        if(rmHR.getLowResMode()) {
+          val = Math.min((rmHR.image[(width/2)*Math.floor(i/2)+Math.floor(j/2)]/rmHR.maxVal)*255,255);
+        } else {
+          val = Math.min((rmHR.image[width*i+j]/rmHR.maxVal)*255,255);
+        }
         imgdata.data[idx0] = Math.floor(val);
         imgdata.data[idx0+1] = Math.floor(val);
         imgdata.data[idx0+2] = Math.floor(val);
@@ -39,31 +44,37 @@ window.onload = function() {
         case 'H':
           rmHR.rotateX(theta);
           rmHR.reset();
+          m=0;
           break;
         case 'l':
         case 'L':
           rmHR.rotateX(-theta);
           rmHR.reset();
+          m=0;
           break;
         case 'j':
         case 'J':
           rmHR.rotateY(theta);
           rmHR.reset();
+          m=0;
           break;
         case 'k':
         case 'K':
           rmHR.rotateY(-theta);
           rmHR.reset();
+          m=0;
           break;
         case 'n':
         case 'N':
           rmHR.rotateZ(theta);
           rmHR.reset();
+          m=0;
           break;
         case 'm':
         case 'M':
           rmHR.rotateZ(-theta);
           rmHR.reset();
+          m=0;
           break;
       }
     }
@@ -71,16 +82,16 @@ window.onload = function() {
 
   function main(tf) {
     m++;
-    
     window.requestAnimationFrame(main);
-
-
     rmHR.renderNextPixels();
-    
-    if(m%6===0) {
+    if(m % 6 === 0) {
       buildImg();
     }
     context.putImageData(imgdata,0,0);
+
+    if(m === 300) { // set high res after five seconds
+      rmHR.setHighResMode();
+    }
   }
 
   main(0);
