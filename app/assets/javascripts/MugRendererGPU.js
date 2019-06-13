22c88e31351b4f2bbf24100a6e85e4a79f0c4b16
ryanden2018 <denlinger.ryan@gmail.com>
2019-06-13 00:44:04 -0500

    full 3d model renders

--- app/assets/javascripts/MugRendererGPU.js
+++ app/assets/javascripts/MugRendererGPU.js
@@ -1,8 +1,8 @@
 
 
-var makeImageCallback = function(randArray,spheres,numSpheres) {
-  var numPhotons = 1000;
-  var randArrayIdx = 0;
+var makeImageCallback = function(randArray,randArrayLength,spheres,numSpheres,cones,numCones,annuli,numAnnuli,Rmat) {
+  var numPhotons = 10;
+  var randArrayIdx = (this.thread.y*this.thread.x*1000)%randArrayLength;
   var maxBounces = 5; 
   var width = 600;
   var val = 0.0;
@@ -14,10 +14,27 @@ var makeImageCallback = function(randArray,spheres,numSpheres) {
     var x = 0.0;
     var y = 0.0;
     var z = 24.0;
+
+    var xb = Rmat[0]*x + Rmat[1]*y + Rmat[2]*z;
+    var yb = Rmat[3]*x + Rmat[4]*y + Rmat[5]*z;
+    var zb = Rmat[6]*x + Rmat[7]*y + Rmat[8]*z;
+
+    x = xb;
+    y = yb;
+    z = zb;
+
     var vx = 1.6*(.25-(.5*this.thread.y)/width);
     var vy = 1.6*(-0.25+(.5*this.thread.x)/width);
     var vz = -1.0;
 
+
+    var vxb = Rmat[0]*vx + Rmat[1]*vy + Rmat[2]*vz;
+    var vyb = Rmat[3]*vx + Rmat[4]*vy + Rmat[5]*vz;
+    var vzb = Rmat[6]*vx + Rmat[7]*vy + Rmat[8]*vz;
+
+    vx = vxb;
+    vy = vyb;
+    vz = vzb;
 
     // iterate over bounces for this photon
     for( var numBounces = 0; numBounces < maxBounces; numBounces++) {
@@ -84,6 +101,118 @@ var makeImageCallback = function(randArray,spheres,numSpheres) {
       }
 
 
+      // handle cones
+      for(var i = 0; i < numCones; i++) {
+        var a = vx*vx+vy*vy-cones[i][1]*cones[i][1]*vz*vz;
+        var b = 2*vx*x + 2*vy*y - 2*(cones[i][0]+cones[i][1]*z)*cones[i][1]*vz;
+        var c = x*x + y*y - Math.pow(cones[i][0]+cones[i][1]*z,2);
+        if(b*b < 4*a*c) { continue; }
+        if(Math.abs(a) <= 0.0001) { continue; }
+      
+        var t1 = (-b + Math.sqrt(b*b-4*a*c)) / (2*a);
+        var t2 = (-b - Math.sqrt(b*b-4*a*c)) / (2*a);
+      
+        var t = -1;
+        if( (t1<0) && (t2<0) ) { continue; }
+      
+        if( (t1<0) && (t2>0) ) { t = t2; }
+      
+        if( (t1>0) && (t2<0) ) { t = t1; }
+      
+        if( (t1>0) && (t2>0) ) {
+          var tmin = Math.min(t1,t2);
+          var tmax = Math.max(t1,t2);
+          if((z + vz*tmin < cones[i][2]) || (z+vz*tmin > cones[i][3])) {
+            t=tmax;
+          } else {
+            t=tmin;
+          }
+        }
+
+        if( t === -1 ) { continue; }
+      
+        if( Math.sqrt(vx*vx+vy*vy+vz*vz)*t < 0.01 ) {
+          continue;
+        }
+      
+        var xp = x+vx*t;
+        var yp = y+vy*t;
+        var zp = z+vz*t;
+      
+        if( zp < cones[i][2] ) { continue; }
+        if( zp > cones[i][3] ) { continue; }
+      
+        var nx = cones[i][4]*x / Math.sqrt(x*x+y*y);
+        var ny = cones[i][4]*y / Math.sqrt(x*x+y*y);
+        var nz = -1 * cones[i][4]*cones[i][1];
+        var nabs = Math.sqrt(nx*nx+ny*ny+nz*nz);
+        nx /= nabs;
+        ny /= nabs;
+        nz /= nabs;
+      
+      
+        if(vx*nx+vy*ny+vz*nz > 0.0) { continue; }
+      
+        if( (minDist === -1) || (Math.sqrt(vx*vx+vy*vy+vz*vz)*t<minDist) ) {
+          minDist = Math.sqrt(vx*vx+vy*vy+vz*vz)*t;
+          nextX = xp;
+          nextY = yp;
+          nextZ = zp;
+          nextNX = nx;
+          nextNY = ny;
+          nextNZ = nz;
+          nextID = cones[i][5];
+        }
+      }
+
+
+
+      // handle annuli
+
+      for(var i = 0; i < numAnnuli; i++) {
+
+        if( vz === 0.0 ) {
+          continue;
+        }
+      
+        var t = (annuli[i][2]-z)/vz;
+      
+        if((t<0) || (Math.sqrt(vx*vx+vy*vy+vz*vz)*t < 0.01)) {
+          continue;
+        }
+      
+        var xp = x + vx*t;
+        var yp = y + vy*t;
+        var zp = z + vz*t;
+      
+        var nx = 0.0;
+        var ny = 0.0;
+        var nz = annuli[i][3];
+      
+        if( Math.sqrt( xp*xp + yp*yp ) > annuli[i][1] ) {
+          continue;
+        }
+      
+        if( Math.sqrt( xp*xp + yp*yp ) < annuli[i][0] ) {
+          continue;
+        }
+      
+      
+        if(vx*nx+vy*ny+vz*nz > 0.0) { continue; }
+        if( (minDist === -1) || (Math.sqrt(vx*vx+vy*vy+vz*vz)*t<minDist) ) {
+          minDist = Math.sqrt(vx*vx+vy*vy+vz*vz)*t;
+          nextX = xp;
+          nextY = yp;
+          nextZ = zp;
+          nextNX = nx;
+          nextNY = ny;
+          nextNZ = nz;
+          nextID = annuli[i][4];
+        }
+      }
+
+
+
       // check if an object was encountered
       if(minDist === -1) { break; }
 
@@ -91,14 +220,14 @@ var makeImageCallback = function(randArray,spheres,numSpheres) {
       y=nextY;
       z=nextZ;
 
-      var vxr = 2*(randArray[randArrayIdx++]-0.5);
-      var vyr = 2*(randArray[randArrayIdx++]-0.5);
-      var vzr = 2*(randArray[randArrayIdx++]-0.5);
+      var vxr = 2*(randArray[(randArrayIdx++)%randArrayLength]-0.5);
+      var vyr = 2*(randArray[(randArrayIdx++)%randArrayLength]-0.5);
+      var vzr = 2*(randArray[(randArrayIdx++)%randArrayLength]-0.5);
       var c = 0;
       while((c<20) && (vxr*nextNX+vyr*nextNY+vzr*nextNZ < 0.0) ) {
-        vxr = 2*(randArray[randArrayIdx++]-0.5);
-        vyr = 2*(randArray[randArrayIdx++]-0.5);
-        vzr = 2*(randArray[randArrayIdx++]-0.5);
+        vxr = 2*(randArray[(randArrayIdx++)%randArrayLength]-0.5);
+        vyr = 2*(randArray[(randArrayIdx++)%randArrayLength]-0.5);
+        vzr = 2*(randArray[(randArrayIdx++)%randArrayLength]-0.5);
         c++;
       }
       if(c === 20) { break; }
@@ -109,7 +238,7 @@ var makeImageCallback = function(randArray,spheres,numSpheres) {
       var uy = vy - 2*nextNY*dotprod;
       var uz = vz - 2*nextNZ*dotprod;
     
-      var lambda = Math.pow( randArray[randArrayIdx++], 2);
+      var lambda = Math.pow( randArray[(randArrayIdx++)%randArrayLength], 2);
     
       vx = lambda*ux+(1.0-lambda)*vxr;
       vy = lambda*uy+(1.0-lambda)*vyr;
--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -2,8 +2,9 @@ window.onload = function() {
 
   document.body.style.background = "black";
   var gpu = new GPU();
+  var Rmat = [1.0,0.0,0.0, 0.0,1.0,0.0, 0.0,0.0,1.0]; 
   var spheres = [
-    [0.0,1.5*75.0,1.5*60.0,30.0,1,0], // [xc,yc,yz,r,lambda,id]
+    [0.0,1.5*75.0,1.5*60.0,30.0,1,0], // [xc,yc,zc,r,lambda,id]
     [0.0,-1.5*75.0,1.5*60.0,30.0,1,-1],
     [1.5*75.0,0.0,1.5*60.0,30.0,1,-2],
     [-1.5*75.0,0.0,1.5*60.0,30.0,1,-3],
@@ -15,31 +16,154 @@ window.onload = function() {
     [7.985,0.0,0.975,1.0,1,6],
     [7.985,0.0,-0.975,1.0,1,6]
   ];
-  var randArray = [];
-  for( var i = 0; i < 400000; i++) { randArray.push(Math.random()); }
+  var cones = [
+    [3.75,0,-4.0,4.0,1,1], // [r0,k,z0,z1,lambda,id]
+    [3.25,0,-3.5,4.0,-1,2]
+  ];
+  var annuli = [
+    [0.0,3.75,-4.0,-1,3], // [r0,r1,z0,lambda,id]
+    [0.0,3.25,-3.5,1,4],
+    [3.25,3.75,4.0,1,5],
+  ];
   var makeImage = gpu.createKernel(makeImageCallback).setOutput([600,600]);
-  var img = makeImage(randArray,spheres,spheres.length);
+  var randArray = [];
+  for( var i = 0; i < 1299827; i++) { randArray.push(Math.random()); }
+ 
+  var img = makeImage(randArray,randArray.length,spheres,spheres.length,cones,cones.length,annuli,annuli.length,Rmat);
 
   var canvas = document.querySelector("#rm");
   var context = canvas.getContext("2d");
   var width = canvas.width;
   var height = canvas.height;
+  var numDraws = 0;
 
   var imgdata = context.createImageData(width,height);
 
+  var redraw = function() {
+    randArray = [];
+    for( var i = 0; i < 1299827; i++) { randArray.push(Math.random()); }
+    var newImg = makeImage(randArray,randArray.length,spheres,spheres.length,cones,cones.length,annuli,annuli.length,Rmat);
 
-  for(var i=0; i<width; i++) {
-    for(var j=0; j<width; j++) {
-      var idx0 = (i*width+j)*4;
-      var val = Math.min(7.5*img[i][j]*255,255);
-      imgdata.data[idx0] = Math.floor(val);
-      imgdata.data[idx0+1] = Math.floor(val);
-      imgdata.data[idx0+2] = Math.floor(val);
-      imgdata.data[idx0+3] = 255;
+    for(var i=0; i<width; i++) {
+      for(var j=0; j<width; j++) {
+        var idx0 = (i*width+j)*4;
+        img[i][j] = (numDraws*img[i][j]+newImg[i][j])/(numDraws+1);
+        var val = Math.min(7.5*img[i][j]*255,255);
+        imgdata.data[idx0] = Math.floor(val);
+        imgdata.data[idx0+1] = Math.floor(val);
+        imgdata.data[idx0+2] = Math.floor(val);
+        imgdata.data[idx0+3] = 255;
+      }
+    }
+
+    numDraws++;
+
+    context.putImageData(imgdata,0,0);
+  }
+
+  var reset = function() {
+    img = makeImage(randArray,randArray.length,spheres,spheres.length,cones,cones.length,annuli,annuli.length,Rmat);
+    numDraws = 0;
+  }
+
+
+  redraw();
+
+
+  var rotateX = function(theta) {
+    var Mat = [1.0,0.0,0.0,
+              0.0,Math.cos(theta),Math.sin(theta),
+              0.0,-Math.sin(theta),Math.cos(theta)];
+
+    Rmat = matmul(Mat,Rmat);
+    return Mat;
+  };
+
+  var rotateY = function(theta) {
+    var Mat = [Math.cos(theta),0.0,-Math.sin(theta),
+              0.0,1.0,0.0,
+              Math.sin(theta),0.0,Math.cos(theta)];
+
+    Rmat = matmul(Mat,Rmat);
+    return Mat;
+  };
+
+  var rotateZ = function(theta) {
+    var Mat = [Math.cos(theta),Math.sin(theta),0.0,
+              -Math.sin(theta),Math.cos(theta),0.0,
+              0.0,0.0,1.0];
+
+    Rmat = matmul(Mat,Rmat);
+    return Mat;
+  };
+
+  var rotateSources = function(Mat) {
+    for( var i = 0; i < spheres.length; i++) {
+      if(spheres[i][5] <= 0) {
+        var xp = Mat[0]*spheres[i][0] + Mat[1]*spheres[i][1] + Mat[2]*spheres[i][2];
+        var yp = Mat[3]*spheres[i][0] + Mat[4]*spheres[i][1] + Mat[5]*spheres[i][2];
+        var zp = Mat[6]*spheres[i][0] + Mat[7]*spheres[i][1] + Mat[8]*spheres[i][2];
+        spheres[i][0] = xp;
+        spheres[i][1] = yp;
+        spheres[i][2] = zp;
+      }
     }
   }
 
-  context.putImageData(imgdata,0,0);
+  document.body.addEventListener("keyup", 
+    function(e) {
+      var theta = Math.PI/32;
+      switch(e.key) {
+        case 'h':
+        case 'H':
+          rotateSources(rotateX(theta));
+          reset();
+          redraw();
+          break;
+        case 'l':
+        case 'L':
+          rotateSources(rotateX(-theta));
+          reset();
+          redraw();
+          break;
+        case 'j':
+        case 'J':
+          rotateSources(rotateY(theta));
+          reset();
+          redraw();
+          break;
+        case 'k':
+        case 'K':
+          rotateSources(rotateY(-theta));
+          reset();
+          redraw();
+          break;
+        case 'n':
+        case 'N':
+          rotateSources(rotateZ(theta));
+          reset();
+          redraw();
+          break;
+        case 'm':
+        case 'M':
+          rotateSources(rotateZ(-theta));
+          reset();
+          redraw();
+          break;
+      }
+    }
+  );
+  
+  var m = 0;
+  function main(tf) {
+    window.requestAnimationFrame(main);
+    if((m++)%60===0) {
+      redraw();
+    }
+  }
+
+  main(0);
+
 
 /****************
    var canvas = document.querySelector("#rm");
