10ea7e5be1ab22434b0d6cc421a079d74f1b44e2
ryanden2018 <39482487+ryanden2018@users.noreply.github.com>
2019-06-20 21:00:27 -0500

    Update MugRendererGPU.js

--- app/assets/javascripts/MugRendererGPU.js
+++ app/assets/javascripts/MugRendererGPU.js
@@ -234,7 +234,7 @@ function computeImage(Rmat,width,numPhotons,maxBounces,mouseIsDown) {
         var u1 = vy - 2*ny*dotprod
         var u2 = vz - 2*nz*dotprod
 
-        if((q+l)%18 === 17) {
+        if( ((q+l)%18 === 17) || ((q+l)%18 === 16) ) {
           Vvec = [u0,u1,u2,numBounces+1];
         } else {
           Vvec = [vxr,vyr,vzr,numBounces+1];
